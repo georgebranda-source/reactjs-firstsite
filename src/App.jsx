c9ce58d0f5a673f5584a424acbc36210d42f03a0
georgebranda-source <georgebranda@gmail.com>
2026-02-10 21:53:19 +0800

    About page and graph backgrounds

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { use, useEffect, useState } from "react";
 import Chart from './components/Chart.jsx';
 import { load, normalizeMatrix, sourceData } from './utils.js';
 import "./App.css";
-import about from "./assets/about.txt?raw";
+import sourceinfo from "./assets/sourceinfo.txt?raw";
+import motivation from "./assets/motivation.txt?raw";
 import beaver from './assets/beaver.png';
 import loonie from './assets/loonie.png';
 
 function App() {
+  const [currentTab, setCurrentTab] = useState("home");
+
   const [data, setData] = useState([]);
   const [realMatrix, setRealMatrix] = useState([]);
   const [normalizedMatrix, setNormalizedMatrix] = useState([]);
@@ -30,8 +33,8 @@ function App() {
   {/* Once data is initialized, update data whenever indicator or range is set */}
   useEffect(() => {
     if (Object.keys(data).length > 0) {
-      setRealMatrix(sourceData({indicator, range, data}));
-      setNormalizedMatrix(normalizeMatrix(sourceData({indicator, range, data})));
+      setRealMatrix(sourceData({ indicator, range, data }));
+      setNormalizedMatrix(normalizeMatrix(sourceData({ indicator, range, data })));
     }
   }, [indicator, range, data]);
 
@@ -42,115 +45,120 @@ function App() {
 
   return (
     <div>
-      <header style={{display: "flex", alignItems: "center", padding: "1rem"}}>
-        <h1 style={{}}> Canadian International Trade Dashboard </h1>
-        <img src={loonie} alt="Loonie" style= {{width: "100px", height: "100px", marginLeft: "1rem"}}/>
-    </header>
-      <label style={{marginLeft: "2rem"}}>
-        Select range:{" "}
-        <select value={range} onChange={(e) => setRange(e.target.value)}>
-          <option value="1y">Past Year</option>
-          <option value="2y">Past 2 Years</option>
-          <option value="5y">Past 5 Years</option>
-          <option value="10y">Past 10 Years</option>
-          <option value="all">All Available Data</option>
-        </select>
-      </label>
+      <header style={{ display: "flex", alignItems: "center", padding: "1rem" }}>
+              <h1> Canadian International Trade Dashboard </h1>
+              <img src={loonie} alt="Loonie" style={{ width: "100px", height: "100px", marginLeft: "1rem" }} />
+      </header>
+      
+      <nav style={{marginLeft: "2rem"}}>
+        <button onClick={() => setCurrentTab("home")}>Home</button>
+        <button onClick={() => setCurrentTab("about")}>About</button>
+      </nav>
 
-      <label style={{marginLeft: "2rem"}}>
-        Select indicator:{" "}
-        <select value={indicator} onChange={(e) => setIndicator(e.target.value)}>
-          <option value="aggregate">Aggregate</option>
-          <option value="exports">Exports</option>
-          <option value="imports">Imports</option>
-        </select>
-      </label>
+      <div style={{ padding: "1rem" }}>
+        {currentTab === "home" && (
+          <div>
+            <label style={{ marginLeft: "2rem" }}>
+              Select range:{" "}
+              <select value={range} onChange={(e) => setRange(e.target.value)}>
+                <option value="1y">Past Year</option>
+                <option value="2y">Past 2 Years</option>
+                <option value="5y">Past 5 Years</option>
+                <option value="10y">Past 10 Years</option>
+                <option value="all">All Available Data</option>
+              </select>
+            </label>
 
-      <div style={{marginLeft: "2rem",marginTop: "1rem"}}>
-        {/* Toggle boxes for countries */}
-        <label>
-            <input type="checkbox" checked={showChina} onChange={() => setShowChina(!showChina)} />
-            Show China Data
-        </label>
-        <label>
-            <input type="checkbox" checked={showUS} onChange={() => setShowUS(!showUS)} />
-            Show US Data
-        </label>
-        <label> 
-            <input type="checkbox" checked={showEU} onChange={() => setShowEU(!showEU)} />
-            Show EU Data
-        </label>
-        <label>
-            <input type="checkbox" checked={showUK} onChange={() => setShowUK(!showUK)} />
-            Show UK Data
-        </label>
-        <label>
-            <input type="checkbox" checked={showMexico} onChange={() => setShowMexico(!showMexico)} />
-            Show Mexico Data
-        </label>
-      </div>
+            <label style={{ marginLeft: "2rem" }}>
+              Select indicator:{" "}
+              <select value={indicator} onChange={(e) => setIndicator(e.target.value)}>
+                <option value="aggregate">Aggregate</option>
+                <option value="exports">Exports</option>
+                <option value="imports">Imports</option>
+              </select>
+            </label>
 
-      <div style ={{display: "flex", flexWrap: "wrap", gap: "2rem", marginLeft: "2rem"}}>
-        <div className="chart-container" style={{flex: 1, minWidth: 650}}> 
-          <h2> Normalized Trade Data </h2>
-          <p> Displayed in percentage of value at earliest selected date </p>
-          <div className="chart-wrapper"style={{position: "relative", width: 650, height: 400, display:"flex", alignItems: "center", justifyContent: "center"}}>
-            <Chart data={normalizedMatrix} showChina={showChina} showUS={showUS} showEU={showEU} showUK={showUK} showMexico={showMexico}/>
-            {loading && (
-              <div className="loading-overlay"
-                style={{
-                  position: "absolute",
-                  top: 0,
-                  left: 0,
-                  bottom: 0,
-                  right: 0,
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-            >
-              <div className="spinner" style={{ marginRight: "1rem"}}></div>
-              Loading customs data...
+            <div style={{ marginLeft: "2rem", marginTop: "1rem" }}>
+              {/* Toggle boxes for countries */}
+              <label>
+                <input type="checkbox" checked={showChina} onChange={() => setShowChina(!showChina)} />
+                Show China Data
+              </label>
+              <label>
+                <input type="checkbox" checked={showUS} onChange={() => setShowUS(!showUS)} />
+                Show US Data
+              </label>
+              <label>
+                <input type="checkbox" checked={showEU} onChange={() => setShowEU(!showEU)} />
+                Show EU Data
+              </label>
+              <label>
+                <input type="checkbox" checked={showUK} onChange={() => setShowUK(!showUK)} />
+                Show UK Data
+              </label>
+              <label>
+                <input type="checkbox" checked={showMexico} onChange={() => setShowMexico(!showMexico)} />
+                Show Mexico Data
+              </label>
+            </div>
+
+            <div style={{ display: "flex", flexWrap: "wrap", gap: "2rem", marginLeft: "2rem" }}>
+              <div className="chart-container">
+                <h2> Normalized Trade Data </h2>
+                <p> Displayed in percentage of value at earliest selected date </p>
+                <div className="chart-wrapper">
+                  <Chart data={normalizedMatrix} showChina={showChina} showUS={showUS} showEU={showEU} showUK={showUK} showMexico={showMexico}/>
+                  {/*loading && (
+                    <div className="loading-overlay"
+                      style={{
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        bottom: 0,
+                        right: 0,
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <div className="spinner" style={{ marginRight: "1rem" }}></div>
+                      Loading customs data...
+                    </div>
+                  )*/}
+                </div>
               </div>
-            )}
-          </div>
-        </div>
-        <div className="chart-container" style={{flex: 1, minWidth: 650}}>
-          <h2> Real Trade Data </h2>
-          <p> Displayed in millions of Canadian dollars (CAD) </p>
-          <div className="chart-wrapper" style={{position: "relative", width: 650, height: 400, display:"flex", alignItems: "center", justifyContent: "center"}}>
-            <Chart data={realMatrix} showChina={showChina} showUS={showUS} showEU={showEU} showUK={showUK} showMexico={showMexico} />
-            {loading && (
-              <div 
-                style={{
-                  position: "absolute",
-                  top: 0,
-                  left: 0,
-                  bottom: 0,
-                  right: 0,
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-            >
-              <div className="spinner" style={{ marginRight: "1rem"}}></div>
-              Loading customs data...
+              <div className="chart-container">
+                <h2> Real Trade Data </h2>
+                <p> Displayed in millions of Canadian dollars (CAD) </p>
+                <div className="chart-wrapper">
+                  <Chart data={realMatrix} showChina={showChina} showUS={showUS} showEU={showEU} showUK={showUK} showMexico={showMexico} />
+                </div>
               </div>
-            )}
+            </div>
+            <p style={{ marginLeft: "2rem", fontSize: "12px" }}> Statistics Canada. Table 12-10-0011-01  International merchandise trade for all countries and by Principal Trading Partners, monthly (x 1,000,000). </p>
           </div>
-        </div>
-      </div>
+        )}
 
-      <div style={{ whiteSpace: "pre-line", padding: "2rem"}}>
-        {about}
-        <b/>
-        <a style={{marginLeft: "0.25rem"}} href="https://www23.statcan.gc.ca/imdb/p2SV.pl?Function=getSurvey&SDDS=2201" target="_blank" rel="noopener noreferrer">
-        Learn more </a>
+        {currentTab === "about" && (
+          <div style={{ padding: "2rem" }}>
+            <h2> Motivation </h2>
+            <div style={{ whiteSpace: "pre-line", padding: "2rem" }}>
+            {motivation}
+            </div>
+            <h2> Source Information </h2>
+            <div style={{ whiteSpace: "pre-line", padding: "2rem" }}>
+              {sourceinfo}
+              <b/>
+              <a style={{ marginLeft: "0.25rem" }} href="https://www23.statcan.gc.ca/imdb/p2SV.pl?Function=getSurvey&SDDS=2201" target="_blank" rel="noopener noreferrer">
+              Learn more </a>
+              <p style={{marginLeft: "2rem"}}> Created by George Branda for educational and recreational purposes. </p>
+            </div>
+            {/*<img src={beaver} alt="National animal" style={{ width: "100px", height: "100px", marginLeft: "1rem", marginRight: "1rem" }} /> */}
+          </div>
+        )}
       </div>
-      <img src={beaver} alt="National animal" style= {{width: "100px", height: "100px", marginLeft: "1rem",marginRight: "1rem"}}/>
-
     </div>
-  )
+  );
 }
 
 export default App
--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,5 +1,5 @@
 import { formatValues, formatDates } from "../utils.js";
-import { LineChart, Line, XAxis, YAxis, Tooltip, Legend } from "recharts"; 
+import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, Tooltip, Legend } from "recharts"; 
 
 function Chart({ data, showChina, showUS, showEU, showUK, showMexico }) {
     data = formatValues(data);
@@ -9,17 +9,18 @@ function Chart({ data, showChina, showUS, showEU, showUK, showMexico }) {
         <div>
             {/*Building a simple line chart*/}
             <LineChart width={600} height={300} data={data} margin={{left: 0, right: 45, bottom: -15, top: -5}}>
-                <XAxis dataKey="refPer" />
-                <YAxis />
-                <Tooltip 
-                    labelStyle={{ color: '#000000', fontWeight: 'bold' }}
-                />
-                <Legend />
-                {showChina && <Line dataKey="chinaValue" stroke="#e01414" name="China" dot={false} strokeWidth={3}/>}
-                {showUS && <Line dataKey="usValue" stroke="#0000bc" name="US" dot={false} strokeWidth={3}/>}
-                {showEU && <Line dataKey="euValue" stroke="#dede14" name="EU" dot={false} strokeWidth={3}/>}
-                {showUK && <Line dataKey="ukValue" stroke="#800080" name="UK" dot={false} strokeWidth={3}/>}
-                {showMexico && <Line dataKey="mexicoValue" stroke="#008000" name="Mexico" dot={false} strokeWidth={3}/>}
+                    <XAxis dataKey="refPer" />
+                    <YAxis />
+
+                    <Tooltip 
+                        labelStyle={{ color: '#000000', fontWeight: 'bold' }}
+                    />
+                    <Legend />
+                    {showChina && <Line dataKey="chinaValue" stroke="#e01414" name="China" dot={false} strokeWidth={3}/>}
+                    {showUS && <Line dataKey="usValue" stroke="#0000bc" name="US" dot={false} strokeWidth={3}/>}
+                    {showEU && <Line dataKey="euValue" stroke="#dede14" name="EU" dot={false} strokeWidth={3}/>}
+                    {showUK && <Line dataKey="ukValue" stroke="#800080" name="UK" dot={false} strokeWidth={3}/>}
+                    {showMexico && <Line dataKey="mexicoValue" stroke="#008000" name="Mexico" dot={false} strokeWidth={3}/>}
             </LineChart>
         </div>
     )
